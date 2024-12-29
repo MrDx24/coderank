ae3d833a70aeaf8bd4bb9caa3334b0cab6af8e5a
unknown <deepatadiya24@gmail.com>
2024-12-29 13:15:39 +0530

    first commit

---
+++ TaskQueueService/src/test/java/com/coderank/taskqueue/TaskQueueService/TaskQueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderank.taskqueue.TaskQueueService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskQueueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
