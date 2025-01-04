2504d32da57604518256b6d7b4dffbd3ff0fc439
unknown <deepatadiya24@gmail.com>
2025-01-04 22:06:33 +0530

    made changes for docker execution

--- ExecutionService/src/main/java/com/coderank/execution/ExecutionService/config/DockerInitializer.java
+++ ExecutionService/src/main/java/com/coderank/execution/ExecutionService/config/DockerInitializer.java
@@ -16,30 +16,27 @@ public class DockerInitializer {
 
     @PostConstruct
     public void initializeDockerImages() {
-
-        String buildContextPath = new File("").getAbsolutePath()
-                                                        + File.separator + "ExecutionService"
-                                                        + File.separator + "docker";
+        // The docker files will be in /app/ExecutionService/docker inside the container
+        String buildContextPath = "/app/ExecutionService/docker";
 
         // Java
-        String contextJava = buildContextPath + File.separator + "java";
-        String dockerfilePathJava = contextJava + File.separator + "Dockerfile-java";
+        String contextJava = buildContextPath + "/java";
+        String dockerfilePathJava = contextJava + "/Dockerfile-java";
         String imageJava = "coderank-java";
 
         // Python
-        String contextPython = buildContextPath + File.separator + "python";
-        String dockerfilePathPython = contextPython + File.separator + "Dockerfile-python";
+        String contextPython = buildContextPath + "/python";
+        String dockerfilePathPython = contextPython + "/Dockerfile-python";
         String imagePython = "coderank-python";
 
-
         // Javascript
-        String contextJavascript = buildContextPath + File.separator + "javascript";
-        String dockerfilePathJavascript = contextJavascript + File.separator + "Dockerfile-javascript";
+        String contextJavascript = buildContextPath + "/javascript";
+        String dockerfilePathJavascript = contextJavascript + "/Dockerfile-javascript";
         String imageJavascript = "coderank-javascript";
 
         // Ruby
-        String contextRuby = buildContextPath + File.separator + "ruby";
-        String dockerfilePathRuby = contextRuby + File.separator + "Dockerfile-ruby";
+        String contextRuby = buildContextPath + "/ruby";
+        String dockerfilePathRuby = contextRuby + "/Dockerfile-ruby";
         String imageRuby = "coderank-ruby";
 
         try {
